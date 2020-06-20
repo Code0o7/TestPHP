5f94b6499af9a88a7a4e13906d87760d937361b2
code007 <yi_aohehe@163.com>
2020-06-20 18:01:17 +0800

    new

--- index.php
+++ index.php
@@ -5,16 +5,18 @@
 $cmd = 'cp /www/wwwroot/test.yycode.top/BetterZip-4.2.5.zip  /root/test.zip';
 // 执行命令
 //putenv('DYLD_LIBRARY_PATH');
-exec($cmd,$result,$status);
+$re = exec($cmd,$result,$status);
 // 结果
 if ($status == 0){
     // 成功
     echo "成功<br>";
     echo "<pre>";
     var_dump($result);
+    var_dump("结果:".$re);
 }else {
     // 失败
     echo "失败<br>";
     echo "<pre>";
     var_dump($result);
+    var_dump("结果:".$re);
 }
