b3e2bb41fb132bf56039b563d8e714eea31ff5b9
code007 <yi_aohehe@163.com>
2020-06-20 17:53:25 +0800

    new

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
 //$cmd = 'rclone copy  BetterZip-4.2.5.zip  GDSuite:/test/';
 
-$cmd = 'cp ./BetterZip-4.2.5.zip  /root/test.zip';
-
+$cmd = 'cp /www/wwwroot/test.yycode.top/BetterZip-4.2.5.zip  /root/test.zip';
 // 执行命令
 //putenv('DYLD_LIBRARY_PATH');
 exec($cmd,$result,$status);
