880d30f426b54aeddcaaf98ea93c4888f285def6
code007 <yi_aohehe@163.com>
2020-06-21 18:46:17 +0800

    new

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 //$cmd = 'rclone copy  /www/wwwroot/test.yycode.top/BetterZip-4.2.5.zip  GDSuite:/test/';
-$cmd = 'rclone delete GSuite:/blogImages/Test/IMG_0689.JPG';
+$cmd = 'rclone delete GSuite:/blogImages/Test/IMG_0689.JPG -vvv';
 
 //$path = "./test";
 //if (!file_exists($path)){
