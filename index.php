1d29a57e03fc05932661d5f8f7fc447122198d5d
code007 <yi_aohehe@163.com>
2020-06-26 20:03:39 +0800

    new

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 $cmd = 'gclone copy GDSuiteTeam:{1UKB8f4tQaIfUhYFTvIFxbBaVt4dxTel3} GDSuiteTeam:test --drive-server-side-across-configs -P';
+echo $cmd;
+die;
 exec($cmd,$result,$status);
 $success = $status == 0 ? true : false;
 if ($success){
