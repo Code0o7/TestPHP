90f961fad91f3fbc25ee97a151ebbecf9b5e6680
code007 <yi_aohehe@163.com>
2020-07-07 03:29:47 +0800

    new

--- index.php
+++ index.php
@@ -1,25 +1,11 @@
 <?php
 
-$cmd = 'php ./test.php 123 456';
-
-exec($cmd,$result,$status);
-$success = $status == 0 ? true : false;
-if ($success){
-    echo "成功";
-    echo "<pre>";
-    var_dump($result);
+$path = "/www/wwwroot/cloud.yycode.ml/cloudmount/GDSuite/test";
+if (file_exists($path)){
+    echo "存在";
 }else {
-    echo "失败";
-    echo "<pre>";
-    var_dump($result);
+    echo "不存在";
 }
-die;
-
-$path = "/Users/mrchen/Desktop/fileTransferPro.txt";
-$content = file_get_contents($path);
-$content = base64_encode($content);
-
-echo json_encode(["code"=>200,"message"=>"","content"=>$content]);
 
 
 
