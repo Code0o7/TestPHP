d5c394956f9f182aac2cd18ebaae1c9c6a84821c
code007 <yi_aohehe@163.com>
2020-07-07 04:17:54 +0800

    new

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 
 $path = "我的数据/备份数据/db/web_stack_db/all";
-$cmd = "rclone delete GDSuite:".$path;
+$cmd = "rclone lsjson GDSuite:我的数据/备份数据/db/";
 exec($cmd.' 2>&1',$result,$status);
 $success = $status == 0 ? true : false;
+
 if ($success){
-    echo "创建成功";
-}else {
-    echo "创建失败";
+    $fileList = $result;
+    $fileList = implode("",$fileList);
+    $fileList = json_decode($fileList,true);
+    echo "<pre>";
+    var_dump($fileList);
 }
 
 
