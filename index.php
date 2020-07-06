cc88e24cd5915914cf298857aaddea087706cd62
code007 <yi_aohehe@163.com>
2020-07-07 04:03:55 +0800

    new

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $path = "我的数据/备份数据/db/web_stack_db/all/";
-$cmd = "rclone mkdir GDSuite:".$path;
+$cmd = "rclone delete GDSuite:".$path;
 exec($cmd.' 2>&1',$result,$status);
 $success = $status == 0 ? true : false;
 if ($success){
