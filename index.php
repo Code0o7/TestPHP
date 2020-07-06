f5183b0fe97bbfa9896072e27294306621d65482
code007 <yi_aohehe@163.com>
2020-07-07 04:00:31 +0800

    new

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $path = "/www/wwwroot/cloud.yycode.ml/cloudmount/GDSuite/我的数据/备份数据/db/web_stack_db/all/";
-$cmd = "rclone mkdir ".$path;
+$cmd = "rclone delete ".$path;
 exec($cmd.' 2>&1',$result,$status);
 $success = $status == 0 ? true : false;
 if ($success){
