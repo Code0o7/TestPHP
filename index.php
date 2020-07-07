f148346ba7bf298d63b478b759f006a1474d4b4e
code007 <yi_aohehe@163.com>
2020-07-07 23:41:27 +0800

    new

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 
-$path = "我的数据/备份数据/db/";
-$cmd = "rclone moveto /www/wwwroot/test.yycode.ml/test/ GDSuite:".$path;
-exec($cmd.' 2>&1',$result,$status);
-$success = $status == 0 ? true : false;
 
-
-if ($success){
-   echo "成功";
-}else {
-    echo "失败";
-}
 
 
 
