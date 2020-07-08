4cf6a390218bd2b6b9ec929bf7d26287269df147
code007 <yi_aohehe@163.com>
2020-07-08 20:02:38 +0800

    new

--- test.php
+++ test.php
@@ -1,9 +1,14 @@
 <?php
 
-$startTime = $this->getime();
+$startTime = getime();
 for ($i=0;$i<100;$i++){
     print "<!--1234567890#########0#########0#########0#########0#########0#########0#########0#########012345-->";
 }
-$timea = $this->getime() - $startTime;
+$timea = getime() - $startTime;
 
 echo "耗时：".$timea."ms";
+
+function getime(){
+    $t = gettimeofday();
+    return (float)($t['sec'] + $t['usec']/1000000);
+}
