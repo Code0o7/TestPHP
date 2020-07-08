7af1cd7c5e0142cef53c4472c65f5d63c3c83ed7
code007 <yi_aohehe@163.com>
2020-07-08 22:18:38 +0800

    new

--- test.php
+++ test.php
@@ -1,14 +1,8 @@
 <?php
 
-$startTime = getime();
-for ($i=0;$i<100;$i++){
-    print "<!--1234567890#########0#########0#########0#########0#########0#########0#########0#########012345-->";
-}
-$timea = getime() - $startTime;
+$totalMemory = memory_get_usage (true);
+$phpMemory = memory_get_usage ();
 
-echo "耗时：".$timea."ms";
-
-function getime(){
-    $t = gettimeofday();
-    return (float)($t['sec'] + $t['usec']/1000000);
-}
+echo "<pre>";
+var_dump($totalMemory);
+var_dump($phpMemory);
