7c61ac64f78f1ba079a63a52064c3a6dc3ea320f
code007 <yi_aohehe@163.com>
2020-07-15 20:17:48 +0800

    new

--- index.php
+++ index.php
@@ -3,27 +3,36 @@
 $url = "https://c1.4861.net:2020/avid5ef16520fe75d/1500kb/hls/index.m3u8";
 
 $content = getHttpsContent($url);
-file_put_contents("log.txt",$content);
-echo "获取内容长度:".strlen($content);
+file_put_contents("/Users/mrchen/Desktop/data.json",$content);
 
 // 获取https文件内容
 function  getHttpsContent($url){
-    global $logPath;
+    //1. 初始化curl请求
+    $ch = curl_init();
+    //2. 设置请求的服务器地址
+    curl_setopt($ch,CURLOPT_URL,$url);
+    //3. 不管get、post，都跳过证书验证
+    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+    curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+    // ps: 如果目标网页跳转，也跟着跳转
+    // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5000000);
 
-    $stream_opts = [
-        "ssl" => [
-            "verify_peer"=>false,
-            "verify_peer_name"=>false,
-        ]
-    ];
+    //判断是直接将结果显示echo还是return
+    //设置结果返回
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    $result = curl_exec($ch);
+    // 处理重定向的请求
+//    $redirectUrl = curl_getinfo($ch)["redirect_url"];
+//    if ($redirectUrl){
+//        // 请求被重定向了 继续请求重定向
+//        $this -> url = $redirectUrl;
+//        $result = $this -> send($data);
+//    }
 
-    $content = "";
-    try {
-        $content = file_get_contents($url,false, stream_context_create($stream_opts));
-    }catch (Exception $exception){
-        echo "捕获到获取内容错误:";
-    }
+    //4. 关闭资源
+    curl_close($ch);
 
-    return $content;
+    return $result;
 }
 
--- test.php
+++ test.php
@@ -1,8 +1,16 @@
 <?php
 
-$totalMemory = memory_get_usage (true);
-$phpMemory = memory_get_usage ();
+$arr = [["id"=>"2","sort"=>"4"],["id"=>"1","sort"=>"2"],["id"=>"4","sort"=>"1"],["id"=>"3","sort"=>"3"]];
+$sort = [];
+foreach ($arr as $item) {
+    $sort[] = $item["sort"];
+}
+echo "<pre>";
+echo "arr:";
+var_dump($arr);
+echo "sort:";
+var_dump($sort);
+array_multisort($sort, SORT_ASC, $arr);
 
 echo "<pre>";
-var_dump($totalMemory);
-var_dump($phpMemory);
+var_dump($arr);
