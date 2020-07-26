2751dfec9e6f8efb0b29ab3b04493b458429c731
code007 <yi_aohehe@163.com>
2020-07-27 01:51:40 +0800

    new

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $url = "http://cloudmount.yycode.ml/LTheater1/loveTheater/movies/5f1cad284a58a/index.m3u8";
 $cmd = "ffmpeg -i https://XXXX/video.m3u8 -c copy  output.mp4";
-$res = exec($cmd);
+$res = execShell($cmd);
 if ($res["success"]){
     echo "转换成功";
 }else {
@@ -10,7 +10,7 @@ if ($res["success"]){
 }
 
 // 执行shell命令
-function exec($cmd){
+function execShell($cmd){
     exec($cmd.' 2>&1',$result,$status);
     $success = $status == 0 ? true : false;
     return [
