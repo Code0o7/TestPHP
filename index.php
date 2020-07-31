6fa591032e9cbc75f02350166bbf4931f327f057
code007 <yi_aohehe@163.com>
2020-07-31 12:07:58 +0800

    new

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php
 
-$status=get_used_status();
-echo "<pre>";
-var_dump($status);
+echo get_server_memory_usage();
+echo "<bbr>";
+echo get_server_cpu_usage();
+
+function get_server_memory_usage(){
+
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = $mem[2]/$mem[1]*100;
+
+    return $memory_usage;
+}
+
+function get_server_cpu_usage(){
+
+    $load = sys_getloadavg();
+    return $load[0];
+
+}
+
+
 
 function get_used_status(){
     $fp = popen('top -b -n 2 | grep -E "^(Cpu|Mem|Tasks)"',"r");//获取某一时刻系统cpu和内存使用情况
