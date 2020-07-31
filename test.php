027fad36fcb2da1d49095b5b15b0e70a4ded5188
code007 <yi_aohehe@163.com>
2020-07-31 10:33:09 +0800

    new

--- test.php
+++ test.php
@@ -1,25 +1,3 @@
 <?php
 
 
-
-die;
-
-$result = mysqli_query($link,$sql);
-echo "<pre>";
-var_dump($result);
-if($result){
-    echo "存在";
-}else {
-    echo "不存在";
-}
-die;
-While($row = mysqli_fetch_assoc($result)){
-    $data[] = $row['Database'];
-}
-unset($result, $row);
-mysqli_close();
-
-if (in_array(strtolower($db_name_php), $data))
-    echo '[',$db_name_php,']数据库存在';
-else
-    echo '[',$db_name_php,']数据库不存在';
