2b4aab9b256b19bac0cab225efb5d5dda770e9a3
code007 <yi_aohehe@163.com>
2021-02-07 11:29:48 +0800

    new

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
 
 $str = "document.write(I(\"簾邉\"))精选";
-echo preg_replace("/document.write(.*)/si","",$str);
+echo preg_replace("/document.write(.*)/si","11",$str);
 
-echo "test";
+
 //$path = "/www/wwwroot/res.yycode.ml/blogImages/";
 //$res = getDirInfoInDir($path);
 //echo "<pre>";
