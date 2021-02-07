f94d2f6ce96e739e35010ea946eeded61472766c
code007 <yi_aohehe@163.com>
2021-02-07 11:29:13 +0800

    new

--- test.php
+++ test.php
@@ -1,5 +1,8 @@
 <?php
 
+$str = "document.write(I(\"簾邉\"))精选";
+echo preg_replace("/document.write(.*)/si","",$str);
+
 echo "test";
 //$path = "/www/wwwroot/res.yycode.ml/blogImages/";
 //$res = getDirInfoInDir($path);
